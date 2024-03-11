a464ccbf5b9018564bbdabce0911b97569f9170c
Kim_SuBeen <sbkim8519@gmail.com>
2024-03-11 14:26:42 +0900

    feat: connect with database

--- models/tag_model.py
+++ models/tag_model.py
@@ -2,26 +2,46 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 import ast
+from loguru import logger
 
-# 데이터 불러오기
-data_path = 'data/'
-interaction = pd.read_csv(data_path + "interaction.csv", index_col=0)
-sideinfo_tag = pd.read_csv(data_path + "sideinfo.csv", index_col=0)
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+import json
+    
+# TF-IDF 벡터라이저 초기화
+      
+def db_connection():
+    load_dotenv()
+    client = MongoClient(os.environ.get('MONGO_URI_1'))
 
-# Tag_string 생성
-def str_to_dict(s):
-    try:
-        return ast.literal_eval(s)
-    except ValueError:
-        return {}
+    # 사용할 데이터베이스 선택
+    db = client['playlist_recommendation']
+    return db
 
-sideinfo_tag['tag_string'] = sideinfo_tag['tags'].apply(lambda x: ' '.join(str_to_dict(x).keys()))
+def fetch_and_prepare_data():
+    # 데이터 불러오기
+    db = db_connection()
+    
+    collection = db['Track']
+    documents = collection.find({}, {'tags' : 1, 'track_name' : 1, 'artist_name' : 1, 'uri' : 1})
+    tags, track_names, artist_names, uris = [], [], [], []
+    
+    for doc in documents:
+        tags.append(" ".join(doc['tags']))
+        track_names.append(doc['track_name'])
+        artist_names.append(doc['artist_name'])
+        uris.append(doc['uri'])
 
-# TF-IDF 벡터라이저 초기화 및 태그 데이터 변환
-tfidf_vectorizer = TfidfVectorizer()
-tfidf_matrix = tfidf_vectorizer.fit_transform(sideinfo_tag['tag_string'])
+    return tags, track_names, artist_names, uris
+    
 
-def recommend_songs(input_tags, N=5):
+def recommend_songs(input_tags, N=10):
+    tags, track_names, artist_names, uris = fetch_and_prepare_data()
+    
+    tfidf_vectorizer = TfidfVectorizer()
+    tfidf_matrix = tfidf_vectorizer.fit_transform(tags)
+    
     # 입력 태그 TF-IDF 벡터화
     input_vec = tfidf_vectorizer.transform([input_tags])
 
@@ -32,12 +52,13 @@ def recommend_songs(input_tags, N=5):
     similar_indices = cosine_sim.argsort()[0][-N-1:-1][::-1]
 
     # 추천된 노래의 track_name 출력
-    recommended_track_ids = list(sideinfo_tag.iloc[similar_indices]['track_name'].values)
+    recommended_songs = [(track_names[i], artist_names[i], uris[i]) for i in similar_indices]
 
-    return recommended_track_ids
+    return recommended_songs
+
 
 if __name__ == "__main__":
     # 사용자로부터 input taga 받기
-    input_tags = "pop Love"
+    input_tags = "pop Love life"
     # 입력된 태그로 노래 추천
-    print(f"추천된 노래의 Track ID: {recommend_songs(input_tags)}")
+    logger.info(f"추천된 노래의 Track ID: {recommend_songs(input_tags)}")
