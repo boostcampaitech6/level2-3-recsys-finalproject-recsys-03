664b7167a89bfa197bad8981bc46e6b292d3c046
Seungbin <pacificcatfish41@gmail.com>
2024-03-18 02:48:56 +0900

    feat: apply new name and logo

--- frontend/src/components/views/navBar/navBar.js
+++ frontend/src/components/views/navBar/navBar.js
@@ -3,11 +3,14 @@ import RightMenu from './section/rightMenu';
 import './section/nav.css';
 
 function NavBar() {
+  //frontend/src/components/views/navBar/navBar.js
+  //logo/logo_extract.png
 
   return (
     <nav className="menu" >
       <div className="menu__logo">
-        <a href="/">Suggestify</a>
+        <a href="/">Au-Dionysos</a>
+        <img src='./logo_extract.png'></img>
       </div>
       <br/>
     </nav>
