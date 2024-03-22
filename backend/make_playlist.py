7f251fc3fd2a20f2c21bedb510b93b3abeabe9c7
Seungbin <pacificcatfish41@gmail.com>
2024-03-23 01:15:40 +0900

    feat: first deployment

--- backend/make_playlist.py
+++ backend/make_playlist.py
@@ -36,8 +36,8 @@ def make_playlist(question, uri, tag_list):
     # 유저 시청 이력 수집
     login_user_data = find_document_by_uri(uri)
     # 채팅 입력 -> 적합한 태그 5개 선택
+    print(tag_list)
     input_tag = ', '.join(filter_model(question, tag_list))
-    # print(input_tag)
     middle = time.time()
     song_list = inference(login_user_data, input_tag)
     end = time.time()
