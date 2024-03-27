28b87b74f29de9f7279fe87f702ace11d03829be
siwoo <aksensp477@gmail.com>
2024-03-28 01:17:27 +0900

    add: get new interaction track uris

--- airflow/dags/dataloader.py
+++ airflow/dags/dataloader.py
@@ -1,5 +1,7 @@
-from pymongo import MongoClient
+import boto3
 import pandas as pd
+import json
+from pymongo import MongoClient
 
 def load_data_from_mongodb():
     client = MongoClient("mongodb://your_mongodb_uri")
@@ -9,3 +11,21 @@ def load_data_from_mongodb():
     data = pd.DataFrame(list(collection.find()))
     # 데이터 전처리 (필요한 경우)
     return data
+
+def get_new_interaction_track():
+    client = boto3.client('secretsmanager')
+    response = client.get_secret_value(SecretId='prod/WebBeta/MongoDB')
+    secret = json.loads(response['SecretString'])
+    mongodb_uri = secret['mongodb_uri']
+    
+    client = MongoClient(mongodb_uri)
+    db = client['playlist_recommendation']
+    collection = db['User']
+    
+    data = collection.find({})
+    new_track_uris = set()
+    for user in data:
+        for track_uri in user['new_track']:
+            new_track_uris.add(track_uri)    
+            
+    return list(new_track_uris)
